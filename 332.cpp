bae075903e4ec47593d84e75ea045451b5363a25
NeoMatrix <lanxis2372615@gmail.com>
2016-02-11 21:15:31 +0800

    finished problem 332
    but not understand the reason.

---
+++ 332.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<pair<string, string>> tickets) {
+        for (auto ticket : tickets) {
+            targets[ticket.first].insert(ticket.second);
+        }
+
+        visit("JFK");
+        return vector<string>(route.rbegin(), route.rend());
+    }
+
+private:
+    unordered_map<string, multiset<string>> targets;
+    vector<string> route;
+
+private:
+    void visit(string airport) {
+        while (!targets[airport].empty()) {
+            string next = *targets[airport].begin();
+            targets[airport].erase(targets[airport].begin());
+            visit(next);
+        }
+
+        route.push_back(airport);
+    }
+};
