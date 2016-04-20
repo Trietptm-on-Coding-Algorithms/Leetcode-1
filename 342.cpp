f5e214e00e5adf767207e8bec2529f29d1734e94
NeoMatrix <lanxis2372615@gmail.com>
2016-04-20 19:44:30 +0800

    finished problem 342, but using loop.

--- 342.cpp
+++ 342.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        
+        if (num < 0) {
+            return false;
+        } else {
+            int count = 0;
+            for (int i = 0; i < 16; ++i, num >>= 2) {
+                if ((num & 3) == 1) {
+                    ++count;
+                } else if ((num & 3) != 0) {
+                    return false;
+                }
+            }
+            
+            return count == 1;
+        }
     }
 };
