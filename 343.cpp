7477a52b39c879a5e477e7cba000cdd8e8d20582
NeoMatrix <lanxis2372615@gmail.com>
2016-04-21 11:22:51 +0800

    using DP method to solve problem 343.

--- 343.cpp
+++ 343.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int integerBreak(int n) {
+        vector<int> dp(n + 1, 1);
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i] = max(dp[i], 
+                max(dp[j], j) * max(dp[i-j], i-j));    
+            }
+        }
         
+        return dp.back();
     }
 };
