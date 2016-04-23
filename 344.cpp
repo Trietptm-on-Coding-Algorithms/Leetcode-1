6af0d2ba1dad7d2c03d428dbe71536096f62ad4b
NeoMatrix <lanxis2372615@gmail.com>
2016-04-23 13:25:06 +0800

    finished problem 344, may be needed improve time.

--- 344.cpp
+++ 344.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     string reverseString(string s) {
-        
+        for (int start = 0, end = s.size() - 1;
+            start < end;
+            ++start, --end) {
+                swap(s[start], s[end]) 
+            }
+            
+        return s;
     }
 };
