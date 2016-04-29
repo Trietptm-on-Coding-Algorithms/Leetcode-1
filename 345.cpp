87fcb82dad43b89a953241e4af3bdf94d71f0c93
NeoMatrix <lanxis2372615@gmail.com>
2016-04-29 19:15:22 +0800

    finished problem 345.

--- 345.cpp
+++ 345.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     string reverseVowels(string s) {
+        string vowels = "aeiouAEIOU";
+        int start = 0, end = s.size() - 1;
         
+        while(start < end) {
+            while (vowels.find(s[start]) == string::npos) {
+                ++start;
+            }
+            
+            while (vowels.find(s[end]) == string::npos) {
+                --end;
+            }
+            
+            if (start < end) {
+                swap(s[start++], s[end--]);
+            }        
+        }
+        
+        return s;
     }
 };
