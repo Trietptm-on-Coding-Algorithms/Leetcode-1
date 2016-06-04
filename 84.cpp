b5fc628ee70f6f3551604cca4e8cb897f492d357
NeoMatrix <lanxis2372615@gmail.com>
2016-06-04 08:17:36 +0800

    finished problem 84

--- 84.cpp
+++ 84.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        int result = 0;
+        heights.push_back(0);
         
+        stack<int> index;
+        
+        for (int i = 0; i < heights.size(); ++i) {
+            
+            while (!index.empty() && heights[index.top()] >= heights[i]) {
+                int height = heights[index.top()];
+                index.pop();
+                
+                int sidx = !index.empty() ? index.top() : -1;
+                result = max(result, height * (i - sidx - 1));
+            }
+            
+            index.push(i);
+        }
+        
+        return result;
     }
 };
