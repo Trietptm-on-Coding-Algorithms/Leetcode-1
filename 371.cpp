daaca6c48eced523e8eabfd3475d89b87a25dc00
Neo <lanxis2372615@gmail.com>
2016-07-01 18:42:37 +0800

    finished problem 371

--- 371.cpp
+++ 371.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        
+        if (b == 0) {
+            return a;
+        }
+
+        int result = a ^ b;
+        int carry = (a & b) << 1;
+
+        return getSum(result, carry);
     }
 };
